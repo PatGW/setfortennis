8bf0e55e720258156ca0423bebc85f1e9d63c670
PatGW <trick7000@hotmail.com>
2013-03-18 10:02:50 +0000

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Setfortennis::Application.config.secret_token = '3539bd5c28b037a6daf3463fc0d41fca568b5ec02b6a0127907579c0b67336bb125a205c481c6bcf76dbbe1b2c10c27b78437a0ee46800543d4247e440056adc'
